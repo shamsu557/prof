032aab3eeaabea74849f2250d172b166b6ac46ae
shamsu557 <118239877+shamsu557@users.noreply.github.com>
2024-09-28 07:26:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -454,8 +454,3 @@ app.get('/adminLogout', (req, res) => {
 });
 app.use('/admin', adminRoutes);
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
