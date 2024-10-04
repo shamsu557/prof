a7928ef139db933f70ddba03f101b15451922125
shamsu557 <118239877+shamsu557@users.noreply.github.com>
2024-10-04 20:40:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ const saltRounds = 10; // Define salt rounds for bcrypt hashing
 
 // MySQL session store options
 const dbConfig = {
-       host: process.env.DB_HOST || 'localhost',  // Use environment variable or default
+    host: process.env.DB_HOST || 'localhost',  // Use environment variable or default
     port: process.env.DB_PORT || 3306,                             // Default MySQL port or environment variable
     user: process.env.DB_USER || 'cpses_fr83m9lkfj@localhost',                   // MySQL username from environment
     password: process.env.DB_PASSWORD || '@Shamsu1440',            // MySQL password from environment
     database: process.env.DB_NAME || 'frankama_dbase'        // Database name from environment
 };
+
+// Create a MySQLStore instance
+const sessionStore = new MySQLStore(dbConfig);
 
 // Set up session with MySQL store
 app.use(session({
@@ -32,8 +35,6 @@ app.use(session({
     maxAge: 365 * 24 * 60 * 60 * 1000 // 1 year session expiration (in milliseconds)
   }
 }));
-
-
 
 // Middleware to parse incoming request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
